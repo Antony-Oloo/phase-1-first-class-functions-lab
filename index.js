07dc5d5e5524c56eae324d8739d5e98b2832d201
antony <antoloo254@gmail.com>
2024-09-25 18:22:02 +0100

    Implemented driver functions and fare multipliers

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+// const returnFirstTwoDrivers = function(){
+//     return drivers.slice(0, 2);
+// };
+// const returnLastTwoDrivers = function(drivers) {
+//     return drivers.slice(-2); 
+//   };
+
+// Returns the first two drivers in the array
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Returns the last two drivers in the array
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array that holds both driver functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Higher-order function to create fare multiplier
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  // Function that doubles the fare
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Function that triples the fare
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function that selects drivers based on the provided driver function
+  const selectDifferentDrivers = function(drivers, driverSelector) {
+    return driverSelector(drivers);
+  };
